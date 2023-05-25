aa6a1279a7c8f5cf37fd95f823be465a2f5d5b37
Johannes Raggam <thetetet@gmail.com>
2023-05-25 17:04:13 +0200

    Fix pyjedi to correctly include all system paths.

--- scripts/pyjedi.py
+++ scripts/pyjedi.py
@@ -3,34 +3,12 @@
 #   mkdir -p .vim; ./bin/instance run ../pyjedi.py > .vim/coc-settings.json
 # Or:
 #   mkdir -p .vim; ./bin/zopepy ../pyjedi.py > .vim/coc-settings.json
-
 import json
-import subprocess
-
-cmd = ["pip", "list", "-v", "--format", "json"]
-result = subprocess.run(cmd, stdout=subprocess.PIPE)
-result = result.stdout.decode("utf-8")
-result = json.loads(result)
-
-# Compile the list of paths.
-# If a location ends with ".egg", do not append the name.
-paths = [
-    f'{it["location"]}/{it["name"]}'
-    if it["location"][-4:] != ".egg"
-    else it["location"]
-    for it in result
-]
-
-# import os
-# import sys
-# omelette_paths = []
-# if os.path.exists(os.path.join(os.getcwd(), "parts", "omelette")):
-#     omelette_paths.append(os.path.join(os.getcwd(), "parts", "omelette"))
-#
-# paths = filter(None, set(paths)) + omelette_paths
+import sys
 
 CONF = {
-    "jedi.workspace.extraPaths": sorted(paths),
+    "jedi.workspace.environmentPath": sys.executable,
+    "jedi.workspace.extraPaths": sorted(set(sys.path)),
     "jedi.workspace.symbols.ignoreFolders": [
         ".nox",
         ".tox",
