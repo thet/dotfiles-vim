38ad5aceeb6bd0fa8c57ef0a93db3588d8a4d7d4
Johannes Raggam <thetetet@gmail.com>
2024-12-05 18:02:42 +0100

    nvim-lsp: Set up ruff lsp which is recommended over pylint, pyflakes and flake8.

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -59,6 +59,21 @@ nvim_lsp.ts_ls.setup {
   cmd = { "typescript-language-server", "--stdio" }
 }
 
+
+-- See: https://github.com/astral-sh/ruff-lsp?tab=readme-ov-file#setup
+-- Configure `ruff-lsp`.
+-- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ruff_lsp
+-- For the default config, along with instructions on how to customize the settings
+nvim_lsp.ruff.setup {
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+}
+
+
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
@@ -69,7 +84,7 @@ null_ls.setup({
         --null_ls.builtins.diagnostics.actionlint,
         --null_ls.builtins.diagnostics.ansiblelint,
         --null_ls.builtins.diagnostics.checkmake,
-        null_ls.builtins.diagnostics.codespell,
+        --null_ls.builtins.diagnostics.codespell,
         --null_ls.builtins.diagnostics.commitlint,
         --null_ls.builtins.diagnostics.dotenv_linter,
         null_ls.builtins.diagnostics.editorconfig_checker,
@@ -77,7 +92,7 @@ null_ls.setup({
         --null_ls.builtins.diagnostics.hadolint,
         --null_ls.builtins.diagnostics.markdownlint,
         --null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.pylint,
+        --null_ls.builtins.diagnostics.pylint,
         null_ls.builtins.diagnostics.rstcheck,
         --null_ls.builtins.diagnostics.sqlfluff.with({
         --    extra_args = { "--dialect", "postgres" }, -- change to your dialect
@@ -86,7 +101,7 @@ null_ls.setup({
         null_ls.builtins.diagnostics.tidy,
         -- Formatters
         null_ls.builtins.formatting.black,
-        null_ls.builtins.formatting.codespell,
+        --null_ls.builtins.formatting.codespell,
         null_ls.builtins.formatting.isort,
         --null_ls.builtins.formatting.nginx_beautifier,
         --null_ls.builtins.formatting.pg_format,
